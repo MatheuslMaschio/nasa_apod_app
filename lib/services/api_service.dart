d6b764b5183ebea0df941070b0c0d80a23919aa1
MatheuslMaschio <matheuslmaschi0@hotmail.com>
2024-07-21 15:01:58 -0300

    feat: add api_service

---
+++ lib/services/api_service.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  final String _baseUrl = '';
+  final String _apiKey = 'UhYHeXGBZYkt70tymnZNsaLvX2myONVIyapTT2mP';
+
+  Future<List<dynamic>> fetchApodList() async {
+    final response =
+        await http.get(Uri.parse('$_baseUrl?api_key=$_apiKey&count=10'));
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Falha ao carregar dados');
+    }
+  }
+}
